6e7f22703f0cf1637ff96be36b239371f081ad11
Adonay Pichardo <adopic@Adonays-MacBook-Pro.local>
2021-05-05 19:04:05 -0400

    player checker selection, ~70% done. Started player turn management, ~80% done.

--- Checkers/checkers.py
+++ Checkers/checkers.py
@@ -12,15 +12,16 @@ from os import system, name
 ###################################################################################################
 
 class game_state:
-    end_game = None
-
-    player_turn = 'PLAYER_ONE'
+    end_game    = None
+    player_turn = None
 
 class token:
-    PLAYER_CONTROL = None
+    HIGHLIGHT      = '> <'
     ROW            = None
     COLUMN         = None
+    player_control = None
     neighbors      = []
+    selected       = None
 
     def __init__(self):
         return
@@ -29,15 +30,14 @@ class gameboard:
     VERTICAL_BORDER   = '|'
     HORIZONTAL_BORDER = '-'
     EMPTY_CELL        = ' '
-
-    MAX_WIDTH  = 8
-    MAX_HEIGHT = 8
-    PLAYER_ONE_TOKEN = 'x'
-    PLAYER_ONE_QUEEN = 'X'
-    PLAYER_TWO_TOKEN = 'o'
-    PLAYER_TWO_QUEEN = 'O'
-
-    gameboard = None
+    MAX_WIDTH         = 8
+    MAX_HEIGHT        = 8
+    PLAYER_ONE_TOKEN  = 'x'
+    PLAYER_ONE_QUEEN  = 'X'
+    PLAYER_TWO_TOKEN  = 'o'
+    PLAYER_TWO_QUEEN  = 'O'
+    highlighted_token = (0, 0)
+    gameboard         = None
 
     def __init__(self):
         # Initialize board with spaces
@@ -76,7 +76,7 @@ class gameboard:
         return
 
     def print_board(self):
-        # clear_terminal()
+        clear_terminal()
         for column_index, every_column in enumerate(self.gameboard):
             stdout.write(f'{(" " + (self.HORIZONTAL_BORDER * 3)) * self.MAX_WIDTH}\n')
             stdout.write(f'{self.VERTICAL_BORDER}')
@@ -94,8 +94,24 @@ def clear_terminal():
     else:
         system('clear')
 
-#START PROGRAM
-###################################################################################################
+def select_token(given_gameboard, given_input):
+    stdout.write(f'FROM select_token given_input = {given_input}\n')
+    input()
+    if given_gameboard is None or given_input is None:
+        return False
+
+    given_gameboard.print_board()
+    return True
+
+def update_board(given_gameboard, given_player, given_move):
+    return True
+
+def request_selection(given_game_state):
+    stdout.write(f'{given_game_state.player_turn}, please select checker to move. (W, A, S, D)\n')
+    player_move = ' '
+    while not player_move in 'wasdWASD':
+        player_move = input()
+    return player_move
 
 def main():
     this_game_state = game_state()
@@ -103,9 +119,18 @@ def main():
     my_gameboard = gameboard()
     my_gameboard.print_board()
     while True:
+        this_game_state.player_control = 'Player 1'
+        player_input = request_selection(this_game_state)
+        while not select_token(my_gameboard, player_input):
+            player_input = request_selection(this_game_state)
+
         end_game = True
         if end_game:
             break
 
-#END PROGRAM
+#START PROGRAM
+###################################################################################################
+
 main()
+
+#END PROGRAM
